a915508b8441523abeb1efdae696e1dab5e0b903
Geesan Munasinghe <sugithgeesan@gmail.com>
2024-10-10 12:11:27 +0530

    tailwind setup

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,25 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
-  },
-  daisyui: {
-    themes: [{
-      light: {
-        "primary": "#001475",
-        "secondary": "#f6d860",
-        "accent": "#37cdbe",
-        "neutral": "#3d4451",
-        "base-100": "#ffffff",
-        "base-200": "#eeeeee",
-        "base-300": "#dddddd"
-      },
-    }, "dark"],
-  },
-  plugins: [require('daisyui'),],
+  plugins: [
+    require('daisyui'),
+  ],
 }
-

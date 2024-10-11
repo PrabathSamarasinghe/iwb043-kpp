c4a2af7bda58071cd0a0360e0b36355641472aa8
Geesan Munasinghe <sugithgeesan@gmail.com>
2024-10-11 21:55:06 +0530

    add initial inserts

---
+++ database/initial_insert.sql
@@ -0,0 +1,5 @@
+insert into system_admins(username,password,name) values
+("Geesan123","pass123","Geesan"),
+("Manujaya123","pass123","Manujaya"),
+("Prabath123","pass123","Prabath"),
+("Kaveesha123","pass123","Kaveesha");

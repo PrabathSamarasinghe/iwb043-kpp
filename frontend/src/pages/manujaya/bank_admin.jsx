746af9f2ae11ee9a1ffd8ad3f79ac25d6a70f52e
kalawilapathiragepasindumanujaya <manujaya.22@cse.mrt.ac.lk>
2024-10-15 14:06:42 +0530

    new

--- frontend/src/pages/manujaya/bank_admin.jsx
+++ frontend/src/pages/manujaya/bank_admin.jsx
@@ -1,20 +1,26 @@
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
-
-import './bank_admin.css';
 import Product from './Components/product';
 import Form from './Components/Form';
+import Button from './Components/Button';
+import Modal from './Components/Modal';
+import './bank_admin.css';
 import { productData, responsive } from './data';
+import { useState } from 'react';
 function bank_admin(){
-    
+    const[OpenModal,setOpenModal]=useState(false);
      const product= productData.map(item=> (<Product name={item.name} number_of_invesments={item.number_of_invesments} discription={item.discription} />));
     return(<div className='bank_admin'>
-      <h1 className='topic'>Bank List</h1>
+      <div className='mcenter'><h1 className='mtopic'>Bank List</h1></div>
       <Carousel showDots={true} responsive={responsive}>
         
         {product}
         
         
-      </Carousel><Form></Form></div>);
+      </Carousel><hr /><Form></Form><hr />
+     <div className='mopen'>
+<button className="mmbtn" onClick={()=>{setOpenModal(true);}}>Update Bank Interest Rates</button>
+</div> 
+     {OpenModal && <Modal closeModal={setOpenModal}/>}</div>);
 }
 export default bank_admin;

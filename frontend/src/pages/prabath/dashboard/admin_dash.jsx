8dceafa040d3fb14cc9d5ce38bade8df35270942
Prabath Samarasinghe <prabhaths2001@gmail.com>
2024-10-15 16:45:37 +0530

    upto date work
    small changes done to the project

---
+++ frontend/src/pages/prabath/dashboard/admin_dash.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './dash.css'
+const admin_dash = () => {
+  return (
+    <main>
+        <div className='side'>
+            <div className='sside'>
+                <h1>Bank Request</h1>
+            </div>
+            <div className='sside'>
+                <h1>User Request</h1>    
+            </div>
+        </div>
+    </main>
+  )
+}
+
+export default admin_dash

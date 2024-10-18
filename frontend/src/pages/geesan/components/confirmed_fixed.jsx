a5578a2fd10718d9afb3049ed1caefb27d111424
Geesan Munasinghe <sugithgeesan@gmail.com>
2024-10-18 17:40:01 +0530

    Added more functionality

--- frontend/src/pages/geesan/components/confirmed_fixed.jsx
+++ frontend/src/pages/geesan/components/confirmed_fixed.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import './cards.css'; // Import the CSS file
 import logo from "/public/logo.jpg";
-const Confirmed_fixed = () => {
+const Confirmed_fixed = ({ depositId, requestedDate, period, amount, bank, productName}) => {
   return (
     <div className="some_card conf">
-       <h6>Deposite_ID : 121</h6>
+       <h6>Deposite_ID : {depositId}</h6>
        <img src={logo} alt="Card Image" className="card_image" />
-       <p><h7 className='card_fieldName'>Requested Date</h7> : 2/3/2022</p>
-       <p><h7 className='card_fieldName'>Period :</h7> 6 months</p>
-       <p><h7 className='card_fieldName'>Amount :</h7> Rs. 100000</p>
-       <p><h7 className='card_fieldName'>Bank :</h7> ABC</p>
+       <p><h7 className='card_fieldName'>Product:</h7> : {productName}</p>
+       <p><h7 className='card_fieldName'>Requested Date</h7> : {requestedDate}</p>
+       <p><h7 className='card_fieldName'>Period :</h7> {period} months</p>
+       <p><h7 className='card_fieldName'>Amount :</h7> Rs. {amount}</p>
+       <p><h7 className='card_fieldName'>Bank :</h7> {bank}</p>
        <p className='card_typedef'>Confirmed</p>
     </div>
   );

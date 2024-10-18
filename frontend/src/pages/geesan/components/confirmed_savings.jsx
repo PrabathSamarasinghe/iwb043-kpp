251372702e2e0759ca32785c693048ef7f0166e6
Geesan Munasinghe <sugithgeesan@gmail.com>
2024-10-18 12:10:44 +0530

    Extended user dashboard

---
+++ frontend/src/pages/geesan/components/confirmed_savings.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './cards.css'; // Import the CSS file
+import logo from "/public/logo.jpg";
+
+const Confirmed_savings = () => {
+  return (
+    <div className="some_card conf">
+       <h6>Deposite_ID : 121</h6>
+       <img src={logo} alt="Card Image" className="card_image" />
+       <p><h7 className='card_fieldName'>Requested Date</h7> : 2/3/2022</p>
+       <p><h7 className='card_fieldName'>Amount :</h7> Rs. 100000</p>
+       <p><h7 className='card_fieldName'>Bank :</h7> ABC</p>
+       <p className='card_typedef'>Confirmed</p>
+    </div>
+  );
+};
+
+export default Confirmed_savings;
--- frontend/src/pages/geesan/components/investBoard.jsx
+++ frontend/src/pages/geesan/components/investBoard.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import './investboard.css'
+import Confirmed_fixed from './confirmed_fixed'
+import Unconfirmed_fixed from './unconfirmed_fixed'
+import Unconfirmed_savings from './unconfirmed_savings'
+import Confirmed_savings from './confirmed_savings'
+import Fixed_new from './fixed_new'
+import Savings_new from './savings_new'
+
 export const InvestBoard = () => {
   return (
     <div className='mainContainer'>
@@ -7,11 +14,29 @@ export const InvestBoard = () => {
         <div className='box'>
             <h2>Fixed Deposites</h2>
             <div className='subBoard'>
+              <Unconfirmed_fixed/>
+              <Unconfirmed_fixed/>
+              <Unconfirmed_fixed/>
+              <Fixed_new/>
+            </div>
+            <div className='subBoard'>
+              <Confirmed_fixed/>
+              <Confirmed_fixed/>
+              <Confirmed_fixed/>
+              <Confirmed_fixed/>
             </div>
         </div>
         <div className='box'>
         <h2>Savings Deposites</h2>
         <div className='subBoard'>
+          <Unconfirmed_savings/>
+          <Unconfirmed_savings/>
+          <Savings_new/>
+        </div>
+        <div className='subBoard'>
+          <Confirmed_savings/>
+          <Confirmed_savings/>
+          <Confirmed_savings/>
         </div>
         </div>
     </div>

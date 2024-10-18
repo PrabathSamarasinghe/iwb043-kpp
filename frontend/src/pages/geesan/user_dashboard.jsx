6dd2a354f47a74b444c19dc93824efe225cb163f
Geesan Munasinghe <sugithgeesan@gmail.com>
2024-10-18 12:12:32 +0530

    removed datafetching for testing

--- frontend/src/pages/geesan/user_dashboard.jsx
+++ frontend/src/pages/geesan/user_dashboard.jsx
@@ -1,32 +1,9 @@
 import Ribbon from './components/Ribbon'
-import React, { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
 import NotVerified from './components/NotVerified';
 import { InvestBoard } from './components/investBoard';
-import axios from 'axios';
 
 const Dashboard = () => {
-  const navigate = useNavigate();
-  const [verified, setData] = useState(null);
-  const url = 'http://localhost:9090/User/verified';
-
-  useEffect(() => {
-    // Define the async function inside useEffect
-    const fetchVerificationStatus = async () => {
-      try {
-        const response = await axios.get(url,{withCredentials:true});
-        setData(response.data);
-        console.log(response.data);
-      } catch (error) {
-        console.log(error);
-        navigate("/prabath/login_user"); // Navigate on error
-      }
-    };
-
-    // Call the async function
-    fetchVerificationStatus();
-  }, [navigate]); // Add navigate as a dependency
-
   return (
     <div className='Udash'>
       <Ribbon />
